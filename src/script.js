1e0e721df7896a3796b43c237703ebac0235f8e1
Anna Hamann <annasoldih@gmail.com>
2021-08-25 15:29:15 -0300

    iniciando

---
+++ src/script.js
@@ -0,0 +1,43 @@
+// const fetch = require('node-fetch');
+// async function getDataVaccine() {
+//   let requestOptions = {
+//     method: 'GET',
+//     redirect: 'follow'
+//   };
+
+//   fetch("https://api.covid19api.com/live/country/brazil/status/confirmed/date/2021-08-24T13:13:30Z", requestOptions)
+//     .then(response => response.json())
+//     .then(result => console.log(result))
+//     .catch(error => console.log('error', error));
+// }
+// getDataVaccine();
+
+let quoteArray = [];
+let index = 0; 
+let textPosition = 0; 
+let flag = true;
+
+loadQuote = () => {  
+  quoteArray[index] = 'Vacinar-se você DEV';
+}
+
+typewriter = () => {
+  if(flag){
+    loadQuote();
+    quoteArray[index] += ""; 
+    flag = false;
+  }
+
+  document.querySelector("#message").innerHTML = quoteArray[index].substring(0, textPosition) + '<span>\u25AE</span>';
+
+  if(textPosition++ != quoteArray[index].length){
+    setTimeout("typewriter()", 100);
+  }
+  else{
+    quoteArray[index] = ' ';
+    setTimeout("typewriter()", 4000);
+    textPosition = 0;
+    flag = true;
+  }   
+}
+window.addEventListener('load', typewriter);
