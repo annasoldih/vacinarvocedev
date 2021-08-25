a5565ba65066e4b45edf56fcbc2586cb248ccd63
Anna Hamann <annasoldih@gmail.com>
2021-08-25 18:53:49 -0300

    css

--- src/map.js
+++ src/map.js
@@ -1,3 +1,5 @@
+// const preencherDados = require('script.js');
+
 google.charts.load('current', {'packages':['geochart']});
 google.charts.setOnLoadCallback(drawRegionsMap);
 
@@ -229,6 +231,100 @@ function drawRegionsMap() {
 function clean() {
   const defs = getElementById('defs');
   console.log(defs.nextSibling);
-
 }
 clean();
+
+
+
+let quoteArray = [];
+let index = 0; 
+let textPosition = 0; 
+let flag = true;
+
+loadQuote = () => {  
+  quoteArray[index] = 'Vacinar-se você DEV';
+}
+
+typewriter = () => {
+  if(flag){
+    loadQuote();
+    quoteArray[index] += ""; 
+    flag = false;
+  }
+
+  document.querySelector("#message").innerHTML = quoteArray[index].substring(0, textPosition) + '<span>\u25AE</span>';
+
+  if(textPosition++ != quoteArray[index].length){
+    setTimeout("typewriter()", 100);
+  }
+  else{
+    quoteArray[index] = ' ';
+    setTimeout("typewriter()", 4000);
+    textPosition = 0;
+    flag = true;
+ }
+ setTimeout(function(){ 
+  // showme.innerText = 'teste';
+}, 4000);
+}
+window.addEventListener('load', typewriter);
+
+
+const showme = document.getElementById('showme');
+const message = document.getElementById('message');
+function showAll() {
+  setTimeout(function(){ 
+    showme.style.display = 'block';
+    message.style.height = '10%';
+    message.style.fontSize = '1.5em';
+    message.style.paddingTop = '50px';
+    message.style.alignItems = 'flex-start';
+    message.style.position = 'absolute';
+    message.style.alignSelf = 'center';    
+    document.body.style.backgroundImage = "url('yoda-right.jpg')";
+  }, 3000);
+}
+
+const myData = {};
+
+const preencherDados = (dados) => {
+  dados.reduce((acc, dado) => {
+    acc.push({
+      [`${dado.Province}`]: {
+        casos: dado.Confirmed,
+        mortes: dado.Deaths,
+      }
+    })
+    return acc;
+  }, [])
+}
+
+// const placeHolder = document.getElementById('placeholder');
+// placeHolder.addEventListener('click', (target) => {
+//   showThisData(myDados.find((ele) => ele.Province === target.id))
+//   //alterar target.id para como vamos identificar o estado ao ele receber o click
+// });
+
+const showThisData = (provinceObj) => {
+  const newElement = document.createElement('span');
+  newElement.innerHTML = `Nº de casos: ${provinceObj.casos}
+  Nº de mortes: ${provinceObj.mortes}`;
+}
+
+async function getDataVaccine() {
+  let requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+
+  const myDados = fetch("https://api.covid19api.com/live/country/brazil/status/confirmed/date/2021-08-24T13:13:30Z", requestOptions)
+    .then(response => response.json())
+    .then((object) => preencherDados(object))
+    .catch(error => console.log('error', error));
+}
+
+
+window.onload = () => {
+  showAll();
+  getDataVaccine();
+}
